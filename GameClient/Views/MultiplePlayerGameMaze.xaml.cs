827e353b70307cb06b4363b501551e31b0a3a33c
Ofir Bitron <bitrono@yahoo.com>
2017-05-24 10:13:14 +0300

    Deleted comments from the project

---
+++ GameClient/Views/MultiplePlayerGameMaze.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GameClient.ViewModel;
+using GameClient.Model;
+
+namespace GameClient.Views
+{
+    /// <summary>
+    /// Interaction logic for MultiplePlayerGameMaze.xaml
+    /// </summary>
+    public partial class MultiplePlayerGameMaze : Window
+    {
+        private MultiPlayerGameViewModel mpgvm;
+
+        public MultiplePlayerGameMaze()
+        {
+            InitializeComponent();
+            this.mpgvm = new MultiPlayerGameViewModel
+                (new MultiPlayerModel(new SettingsModel()));
+            this.DataContext = this.mpgvm;
+            this.mpgvm.StartNewGame();
+        }
+
+        /// <summary>
+        /// Go back to main menu.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackMainMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+    }
+}
---
+++ GameClient/Views/SinglePlayerMenu.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GameClient.ViewModel;
+using GameClient.Model;
+
+namespace GameClient.Views
+{
+    /// <summary>
+    /// Interaction logic for SinglePlayerMenu.xaml
+    /// </summary>
+    public partial class SinglePlayerMenu : Window
+    {
+
+        private SinglePlayerGameViewModel singlePlayerGameViewModel;
+
+        public SinglePlayerMenu()
+        {
+            InitializeComponent();
+            this.singlePlayerGameViewModel = new SinglePlayerGameViewModel
+                (new SinglePlayerGameModel(), new SettingsModel());
+            this.DataContext = this.singlePlayerGameViewModel;
+        }
+        
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open maze window.
+            new SinglePlayerGameMaze(NumOfRows.Text, numOfCols.Text, MazeNameBox.Text).Show();
+            this.Close();
+        }
+    }
+}
+
